37c18aaadcc648f2ba4376a5d80ee438fb8d77d6
Abbeville <abbeville13@gmail.com>
2024-08-28 09:36:41 +0100

    Obsolete property(rootPath) replaced with workspaceFolders on vscode.workspace

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,52 @@
 import * as fs from 'fs';
+import path from 'path';
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		vscode.commands.registerCommand('extension.listDependencies', async () => {
-			const packageJsonPath = vscode.workspace.rootPath + "/package.json";
-			fs.readFile(packageJsonPath, 'utf-8' ,(err, data) => {
+			const workspaceFolder = vscode.workspace.workspaceFolders;
+			if (!workspaceFolder) {
+				vscode.window.showErrorMessage("No workspace folder is open");
+				return;
+			}
+
+			const packageJsonPath = path.join(workspaceFolder[0].uri.fsPath,  'package.json');
+			fs.readFile(packageJsonPath, 'utf-8', (err, data) => {
 				if (err) {
-					vscode.window.showErrorMessage("Error reading package.json");
+					vscode.window.showErrorMessage("Could not find a package.json file. Please create one");
 				}
 				const packageJson = JSON.parse(data);
 				const dependencies = packageJson.dependencies;
-				vscode.window.showInformationMessage(`Here is ${JSON.stringify(dependencies, null, 2)}`);
+
+				if (!dependencies) {
+					vscode.window.showErrorMessage("Could not find dependencies in the package.json");
+				}
+				vscode.window.showInformationMessage(`${Object.keys(dependencies).join('\n')}`);
 			});
 		}
 	));
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand('extension.listDevDependencies', async () => {
-			const packageJsonPath = vscode.workspace.rootPath + "/package.json";
-			fs.readFile(packageJsonPath, 'utf-8' ,(err, data) => {
+			const workspaceFolder = vscode.workspace.workspaceFolders;
+			if (!workspaceFolder) {
+				vscode.window.showErrorMessage("No workspace folder is open");
+				return;
+			}
+
+			const packageJsonPath = path.join(workspaceFolder[0].uri.fsPath,  'package.json');
+			fs.readFile(packageJsonPath, 'utf-8', (err, data) => {
 				if (err) {
-					vscode.window.showErrorMessage("Error reading package.json");
+					vscode.window.showErrorMessage("Could not find a package.json file. Please create one");
 				}
 				const packageJson = JSON.parse(data);
 				const devDependencies = packageJson.devDependencies;
-				vscode.window.showInformationMessage(`Here is ${JSON.stringify(devDependencies, null, 2)}`);
+
+				if (!devDependencies) {
+					vscode.window.showErrorMessage("Could not find devDependencies in the package.json");
+				}
+				vscode.window.showInformationMessage(`${Object.keys(devDependencies).join('\n')}`);
 			});
 		}
 	));
