2ded349565f7862f2081b3690b52a38137b067c7
Abbeville <abbeville13@gmail.com>
2024-08-28 06:35:19 +0100

    Add commands "List dependencies and DevDependencies"

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,34 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import * as fs from 'fs';
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(
+		vscode.commands.registerCommand('extension.listDependencies', async () => {
+			const packageJsonPath = vscode.workspace.rootPath + "/package.json";
+			fs.readFile(packageJsonPath, 'utf-8' ,(err, data) => {
+				if (err) {
+					vscode.window.showErrorMessage("Error reading package.json");
+				}
+				const packageJson = JSON.parse(data);
+				const dependencies = packageJson.dependencies;
+				vscode.window.showInformationMessage(`Here is ${JSON.stringify(dependencies, null, 2)}`);
+			});
+		}
+	));
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "npm-navigator" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('npm-navigator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from NPM Navigator!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('extension.listDevDependencies', async () => {
+			const packageJsonPath = vscode.workspace.rootPath + "/package.json";
+			fs.readFile(packageJsonPath, 'utf-8' ,(err, data) => {
+				if (err) {
+					vscode.window.showErrorMessage("Error reading package.json");
+				}
+				const packageJson = JSON.parse(data);
+				const devDependencies = packageJson.devDependencies;
+				vscode.window.showInformationMessage(`Here is ${JSON.stringify(devDependencies, null, 2)}`);
+			});
+		}
+	));
 }
 
 // This method is called when your extension is deactivated
